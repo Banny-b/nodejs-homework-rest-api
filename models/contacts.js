67178d7da396675fbbcfcd27d888e953bb42fdd5
Sergey Petukhov <banny75@i.ua>
2023-04-09 11:39:38 +0300

    hw03-mongodb

--- models/contacts.js
+++ models/contacts.js
@@ -1,64 +1,27 @@
-const fs = require("fs/promises");
-const { nanoid } = require("nanoid");
-const path = require("path");
-const contactsPath = path.join(__dirname, "./contacts.json");
+const { Schema, model } = require("mongoose");
 
-async function changeContactsPath(data) {
-  await fs.writeFile(contactsPath, JSON.stringify(data, null, 2));
-};
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
 
-async function listContacts() {
-  const contacts = await fs.readFile(contactsPath);
-  return JSON.parse(contacts);
-};
-
-async function getContactById(contactId) {
-  const contacts = await listContacts();
-  const contact = contacts.find(
-    (contact) => contact.id === contactId.toString()
-  );
-  return contact;
-};
-
-async function removeContact(contactId) {
-  const contacts = await listContacts();
-  const removeID = contacts.findIndex((contact) => contact.id === contactId);
-  if (removeID === -1) {
-    return null;
-  }
-  const [removeContacts] = contacts.splice(removeID, 1);
-  changeContactsPath(contacts);
-    return removeContacts;
-};
-
-async function addContact(body) {
-  const newContact = {
-    id: nanoid(),
-    ...body,
-  };
-  const contacts = await listContacts();
-  contacts.push(newContact);
-  changeContactsPath(contacts);
-    return newContact;
-};
-
-async function updateContact(contactId, body) {
-  const contacts = await listContacts();
-  const updateID = contacts.findIndex(
-    (contact) => contact.id === contactId
-  );
-  if (updateID === -1) {
-    return null;
-  };
-  contacts[updateID] = {...contacts[updateID],...body };
-  await changeContactsPath(contacts);
-    return contacts[updateID];
-};
+const Contact = model("contact", contactSchema);
 
 module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
+  Contact,
 };
